4452544966edf403a500f6c552c9959390ad2891
JayDub21 <justin.wofford.21@gmail.com>
2020-02-19 18:29:58 -0600

    fixed import parth for Employee

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
 import "./App.css";
-import Employee from "./components/employees";
+import Employee from "./components/EmployeeCard/index.jsx";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
 
 function App() {
-  return <Employee />;
+  return (
+    <Wrapper>
+      <Title>Employee Directory</Title>
+      {this.state.friends.map(friend => (
+        <Employee
+          removeFriend={this.removeFriend}
+          id={friend.id}
+          key={friend.id}
+          name={friend.name}
+          image={friend.image}
+          occupation={friend.occupation}
+          location={friend.location}
+        />
+      ))}
+    </Wrapper>
+  );
 }
 
 export default App;
--- employee-directory/src/components/EmployeeCard/index.jsx
+++ employee-directory/src/components/EmployeeCard/index.jsx
@@ -4,8 +4,26 @@ import "./style.css";
 class Employee extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World!</h1>
+      <div className="card">
+        <div className="img-container">
+          <img alt={props.name} src={props.image} />
+        </div>
+        <div className="content">
+          <ul>
+            <li>
+              <strong>Name:</strong> {props.name}
+            </li>
+            <li>
+              <strong>Occupation:</strong> {props.occupation}
+            </li>
+            <li>
+              <strong>Location:</strong> {props.location}
+            </li>
+          </ul>
+        </div>
+        <span onClick={() => props.removeFriend(props.id)} className="remove">
+          𝘅
+        </span>
       </div>
     );
   }
